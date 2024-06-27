179b399302e9cc7f029c23b9550a88f4437bde29
Gerard McLean <gerard.mclean@kainos.com>
2024-06-27 06:21:44 -0700

    Insert statements for delivery employees done

--- src/main/resources/db/migration/V4__create_delivery_employee_table.sql
+++ src/main/resources/db/migration/V4__create_delivery_employee_table.sql
@@ -7,3 +7,36 @@ CREATE TABLE delivery_employee(
     comm_rate decimal(5,2),
     role_id tinyint
 );
+
+
+
+
+INSERT INTO delivery_employee(name, salary, bank_acc_num, ni_num, comm_rate, role_id)
+    VALUES(
+        'Gerard McLean David',
+        '11122.21',
+        '12345678912356780',
+        'PH123456L',
+        '15.00',
+        3
+    );
+
+INSERT INTO delivery_employee(name, salary, bank_acc_num, ni_num, comm_rate, role_id)
+    VALUES(
+        'Tom Phan Wayne',
+        '111224.21',
+        '12345678912347802345',
+        'PH127896L',
+        '15.00',
+        3
+    );
+
+INSERT INTO delivery_employee(name, salary, bank_acc_num, ni_num, comm_rate, role_id)
+    VALUES(
+        'Katie McAllister Bartholomew',
+        '1112223.21',
+        '1234567891234560',
+        'PH123456L',
+        '15.00',
+        3
+    );
