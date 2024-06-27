5b7ea8c82531796c02b2e033aaf413dfab50b449
thomkainos <thomas.phan@kainos.com>
2024-06-27 15:21:37 +0100

    finished migration V2

--- src/main/resources/db/migration/V2__create_project_table.sql
+++ src/main/resources/db/migration/V2__create_project_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE project (
-    proj_id smallint PRIMARY KEY,
+    proj_id smallint auto_increment PRIMARY KEY,
     proj_name varchar(100),
     proj_value decimal(12,2),
     list_tech text,
@@ -8,11 +8,11 @@ CREATE TABLE project (
     proj_creation DATE
 );
 
-INSERT INTO product 
-(proj_id, proj_name, proj_value, list_tech, client_id, curr_status, employee_id, proj_creation)
+INSERT INTO project 
+(proj_name, proj_value, list_tech, client_id, curr_status, proj_creation)
 VALUES 
-    (1, 'Project Alpha', 50000, 'Python, JavaScript, React', 101, 'In Progress', 1001, '2024-06-27'),
-    (2, 'Project Beta', 75000, 'Java, Spring, Angular', 102, 'Completed', 1002, '2024-05-15'),
-    (3, 'Project Gamma', 60000, 'C#, .NET, Vue.js', 103, 'On Hold', 1003, '2024-04-20'),
-    (4, 'Project Delta', 80000, 'Ruby, Rails, PostgreSQL', 104, 'In Progress', 1004, '2024-03-30'),
-    (5, 'Project Epsilon', 45000, 'PHP, Laravel, MySQL', 105, 'Cancelled', 1005, '2024-02-25');
+    ('Project Alpha', 50000, 'Python, JavaScript, React', 101, 'inactive', '2024-06-27'),
+    ('Project Beta', 75000, 'Java, Spring, Angular', 102, 'completed', '2024-05-15'),
+    ('Project Gamma', 60000, 'C#, .NET, Vue.js', 103, 'active','2024-04-20'),
+    ('Project Delta', 80000, 'Ruby, Rails, PostgreSQL', 104, 'inactive', '2024-03-30'),
+    ('Project Epsilon', 45000, 'PHP, Laravel, MySQL', 105, 'completed', '2024-02-25');
