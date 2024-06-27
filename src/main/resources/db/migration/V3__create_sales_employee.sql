6f1ed883b320a3c1e2df2c0ebf28f8640a3d790f
Gerard McLean <gerard.mclean@kainos.com>
2024-06-27 06:19:36 -0700

    Insert statements for sales employees done

--- src/main/resources/db/migration/V3__create_sales_employee.sql
+++ src/main/resources/db/migration/V3__create_sales_employee.sql
@@ -7,3 +7,34 @@ CREATE TABLE sales_employee(
     comm_rate decimal(5,2),
     role_id tinyint
 );
+
+
+INSERT INTO sales_employee(name, salary, bank_acc_num, ni_num, comm_rate, role_id)
+    VALUES(
+        'Gerard McLean',
+        '111222.21',
+        '123456789123456780',
+        'PH123456L',
+        '30.00',
+        2
+    );
+
+INSERT INTO sales_employee(name, salary, bank_acc_num, ni_num, comm_rate, role_id)
+    VALUES(
+        'Tom Phan',
+        '1112224.21',
+        '1234567891234567802345',
+        'PH127896L',
+        '30.00',
+        2
+    );
+
+INSERT INTO sales_employee(name, salary, bank_acc_num, ni_num, comm_rate, role_id)
+    VALUES(
+        'Katie McAllister',
+        '11122223.21',
+        '123456789123456780',
+        'PH123456L',
+        '30.00',
+        1
+    );
