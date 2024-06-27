ba455ce703200a80d27682283e93b4f152fed30f
Gerard McLean <gerard.mclean@kainos.com>
2024-06-27 06:24:43 -0700

    Added NOT NULL constraints to employee details

--- src/main/resources/db/migration/V3__create_sales_employee.sql
+++ src/main/resources/db/migration/V3__create_sales_employee.sql
@@ -1,11 +1,11 @@
 CREATE TABLE sales_employee(
     employee_id smallint PRIMARY KEY AUTO_INCREMENT,
-    name varchar(255),
-    salary decimal(11, 2),
-    bank_acc_num varchar(34),
-    ni_num char(9),
-    comm_rate decimal(5,2),
-    role_id tinyint
+    name varchar(255) NOT NULL,
+    salary decimal(11, 2) NOT NULL,
+    bank_acc_num varchar(34) NOT NULL,
+    ni_num char(9) NOT NULL,
+    comm_rate decimal(5,2) NOT NULL,
+    role_id tinyint NOT NULL
 );
 
 
