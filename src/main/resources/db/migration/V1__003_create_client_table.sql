4407a296e0d9b8f98979b1aacfcbc721eec887fd
Katiemcallister <katie.mcallister@kainos.com>
2024-06-27 14:07:04 +0100

    Commit auto increment for client_id

--- src/main/resources/db/migration/V1__003_create_client_table.sql
+++ src/main/resources/db/migration/V1__003_create_client_table.sql
@@ -6,3 +6,7 @@ CREATE TABLE `Client` (
                             `proj_id` smallint
 
 );
+
+INSERT INTO Client (client_id, name, address, phone_num, proj_id) VALUES (1,'Engineering academy','123 Engineering Street','07890123456',1);
+INSERT INTO Client (client_id, name, address, phone_num, proj_id) VALUES (2,'NASA','100 Space Drive','0000000',2);
+INSERT INTO Client (client_id, name, address, phone_num, proj_id) VALUES (3,'Tim Hortons','72 Vanilla Avenue','077123456789',3);
--- src/main/resources/db/migration/V6__006_create_roles.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `Roles` (
-                          `Role_id` tinyint,
-                          `Role_name` varchar(255),
-
-);
