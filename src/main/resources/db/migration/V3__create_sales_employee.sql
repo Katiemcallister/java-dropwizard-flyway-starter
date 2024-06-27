019698c32b775aa31327a66a235aee8e20a1f7b9
Gerard McLean <gerard.mclean@kainos.com>
2024-06-27 05:50:30 -0700

    Sales employee table creation

---
+++ src/main/resources/db/migration/V3__create_sales_employee.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sales_employee(
+    employee_id smallint PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    salary decimal(11, 2),
+    bank_acc_num varchar(34),
+    ni_num char(9),
+    comm_rate decimal(5,2),
+    role_id tinyint
+);
