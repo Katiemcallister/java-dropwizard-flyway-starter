57cffca8350f232a68394aef54b74ee045d3fd5a
Gerard McLean <gerard.mclean@kainos.com>
2024-06-27 06:25:18 -0700

    Added NOT NULL constraints to employee details

--- src/main/resources/db/migration/V7__create_proj_history_table.sql
+++ src/main/resources/db/migration/V7__create_proj_history_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE proj_history(
     proj_hist_id smallint PRIMARY KEY AUTO_INCREMENT,
-    proj_id smallint,
-    employee_id smallint FOREIGN KEY REFERENCES proj_employee(employee_id)
+    proj_id smallint NOT NULL,
+    employee_id smallint FOREIGN KEY REFERENCES proj_employee(employee_id) NOT NULL
 );
