fec133d1ea8faaa0d17dc9a676570026262aae69
Katiemcallister <katie.mcallister@kainos.com>
2024-07-03 14:36:53 +0100

    Adding metrices to a databse list

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -16,14 +16,18 @@ public class TestDao {
         try (Connection connection = DatabaseConnector.getConnection()) {
             Statement statement = connection.createStatement();
 
+            long start = System.currentTimeMillis();
+
             ResultSet resultSet = statement.executeQuery(
                     "SHOW DATABASES;");
 
+            long end = System.currentTimeMillis();
+
+            System.out.println("Total time to execute query in milliseconds: " + currentTimeMillis());
+
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("Database"));
                 databases.add(resultSet.getString("Database"));
             }
-            System.out.println("Finished retrieving database names");
         }
 
         return databases;
